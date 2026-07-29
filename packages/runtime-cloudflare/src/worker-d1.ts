e04dabce3d7e5a399ecaac10a78c7707fc16a9f0
Chris Huber <chris.huber@automattic.com>
2026-07-28 21:31:07 -0400

    Remove Durable Object fallback from D1 runtime

--- packages/runtime-cloudflare/src/worker-d1.ts
+++ packages/runtime-cloudflare/src/worker-d1.ts
@@ -1,17 +1,12 @@
 import { D1RevisionCoordinator } from "./d1-revision-coordinator.js"
 import { D1OperationRepository } from "./d1-operation-repository.js"
-import { DurableObjectRevisionCoordinator, WordPressStateCoordinator } from "./state-coordinator.js"
-export { WordPressStateCoordinator }
 import type { SiteContext } from "./site-context.js"
 import { createCloudflareRuntime, type RuntimeEnv } from "./worker.js"
 
 interface D1RuntimeEnv extends RuntimeEnv {
   WORDPRESS_STATE_DATABASE: D1Database
-  WORDPRESS_STATE: DurableObjectNamespace
 }
 
 export default createCloudflareRuntime<D1RuntimeEnv>((env, site: SiteContext) => (
   new D1RevisionCoordinator(env.WORDPRESS_STATE_DATABASE, site.id)
-), (env, site: SiteContext, selector) => (
-  selector === "durable-object" ? new DurableObjectRevisionCoordinator(env.WORDPRESS_STATE.getByName(site.id), site.id) : undefined
-), (env) => new D1OperationRepository(env.WORDPRESS_STATE_DATABASE))
+), undefined, (env) => new D1OperationRepository(env.WORDPRESS_STATE_DATABASE))
